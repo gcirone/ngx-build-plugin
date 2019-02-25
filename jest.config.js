6c7513b6a81bfae7c47ee73d155163151d9021c4
Gianluca Cirone <gianluca.cirone@gmail.com>
2019-02-25 22:41:30 +0100

    Add jest as test framework

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+const jestJunitConfig = {
+  outputDirectory: '<rootDir>/build/test-results/jest',
+  outputName: 'results.xml'
+};
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+
+  name: 'ngx-build-plugin',
+  roots: ['<rootDir>/src/'],
+
+  testMatch: ['**/*+(spec|test).+(ts|js)?(x)'],
+  testPathIgnorePatterns: ['/node_modules/', '/build/'],
+
+  collectCoverage: true,
+  coverageReporters: ['lcovonly', 'html', 'text-summary'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/build/'],
+  coverageDirectory: '<rootDir>/build/coverage',
+
+  reporters: ['default', ['jest-junit', jestJunitConfig]],
+  cacheDirectory: '<rootDir>/build/.jest-cache'
+};
---
+++ src/build-plugin.spec.ts
@@ -0,0 +1,46 @@
+import { BuildPlugin } from './build-plugin';
+import * as core from '@angular-devkit/core';
+import * as path from 'path';
+
+jest.mock('root/folder/file.plugin.js', () => ({ pre: jest.fn(value => value) }), { virtual: true });
+jest.mock('@angular-devkit/core');
+jest.mock('path');
+
+describe('BuildPlugin Test', () => {
+  const options = { plugin: 'folder/file.plugin.js' };
+  const root = 'root';
+
+  beforeAll(() => {
+    jest.spyOn(core, 'getSystemPath').mockImplementation(value => value);
+    jest.spyOn(path, 'resolve').mockImplementation((...values) => values.join('/'));
+  });
+
+  describe('#loadPlugin', () => {
+    beforeEach(() => BuildPlugin.loadPlugin(root as any, options));
+
+    it('should resolve the plugin path', () => {
+      expect(core.getSystemPath).toHaveBeenLastCalledWith(root);
+      expect(path.resolve).toHaveBeenLastCalledWith(root, options.plugin);
+    });
+
+    it('should load the plugin correctly', () => {
+      expect((BuildPlugin as any).plugin).toEqual({ pre: expect.any(Function) });
+    });
+  });
+
+  describe('#runHook', () => {
+    const data = { sample: true };
+
+    it('should return the value if the hook method is set up', () => {
+      BuildPlugin.loadPlugin(root as any, options);
+
+      const hookReturnValue = BuildPlugin.runHook('pre', data);
+      expect(hookReturnValue).toEqual(data);
+    });
+
+    it('should return undefined if the hook method is not set up', () => {
+      const hookReturnValue = BuildPlugin.runHook('post', data);
+      expect(hookReturnValue).toBeUndefined();
+    });
+  });
+});
